88e698b38153f68f2ff205952194e864f5321bc6
Loncha Dhaval <163704413+Dhaval-loncha@users.noreply.github.com>
2024-04-10 11:26:55 +0530

    Add files via upload

---
+++ Notes/notes.js
@@ -0,0 +1,16 @@
+function openTab(evt, tabName) {
+    var i, tabContent, tabButtons;
+    tabContent = document.getElementsByClassName("tab");
+    for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
+    }
+    tabButtons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButtons.length; i++) {
+    tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+// By default, open the first tab
+document.getElementById("tab1").style.display = "block";
+document.getElementsByClassName("tab-button")[0].className += " active";
